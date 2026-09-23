eb4ae5147d824476fa2808f770c427a3aa3561ad
Artifait <am3044288@gmail.com>
2026-09-23 18:38:34 +0300

    feat: Убрал шаблонный HelloWorld, зарегал и забиндил кастомную команду

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const disposable = vscode.commands.registerCommand('py-cond-to-cpp.convert', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "py-cond-to-cpp" is now active!');
+        const lineNumber = editor.selection.active.line;
+        const lineRange = editor.document.lineAt(lineNumber).range;
+        const lineText = editor.document.lineAt(lineNumber).text;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('py-cond-to-cpp.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from If/While Fixer!');
-	});
+        const newText = convertCondition(lineText);
 
-	context.subscriptions.push(disposable);
+        if (newText === null) {
+            vscode.window.showErrorMessage(
+                'if/while не найдено в текущей строке'
+            );
+            return;
+        }
+        if (newText === lineText) {
+            vscode.window.showInformationMessage('Условие уже в порядке');
+            return;
+        }
+
+        editor.edit((editBuilder) => {
+            editBuilder.replace(lineRange, newText);
+        });
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+// Null - если не найден if/while
+// Иначе конвертированную строку
+function convertCondition(lineText: string): string | null {
+    return null;
+}
+
+export function deactivate() { }
